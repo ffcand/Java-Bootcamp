bd3460a83b3ad208e417861c8b337eb4ae910987
Andre <and.ls.toga@gmail.com>
2025-03-24 18:21:12 -0300

    Operadores lógicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,23 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeUm = "André";
-        String nomeDois = new String("André");
+        boolean condicao1=true;
+        boolean condicao2=true;
 
-        System.out.println(nomeUm.contentEquals(nomeDois));
-        
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if (numero1 == numero2){ //true
-            System.out.println("a nossa condição é verdadeira");
+        if (condicao1 && (7 >4)){
+            System.out.println("As duas condições são verdadeiras");
         }
-
-        System.out.println("numeroUm é igual a numeroDois? " + simNao);
-
-        simNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+        if (condicao1 || condicao2){
+            System.out.println("Uma das ondições é verdadeira");
+        }
+        System.out.println("Fim");
     }
 }
