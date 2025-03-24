438d4f5766486347a489a46e39c13d682042c91c
Andre <and.ls.toga@gmail.com>
2025-03-24 17:14:02 -0300

    Variáveis vs Constantes.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,5 @@
+import javax.swing.text.StyledEditorKit;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
@@ -5,5 +7,13 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
