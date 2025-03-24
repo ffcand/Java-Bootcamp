1be9f24e2d7fb4ebb8e46841abb654d200bf99ff
Andre <and.ls.toga@gmail.com>
2025-03-24 18:14:01 -0300

    Operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
-        int a, b;
-        a = 5;
-        b = 6;
+        String nomeUm = "André";
+        String nomeDois = new String("André");
 
-        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(nomeUm.contentEquals(nomeDois));
+        
+        int numero1 = 1;
+        int numero2 = 2;
 
-        System.out.println(resultado);
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2){ //true
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
     }
 }
