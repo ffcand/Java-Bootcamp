5f21b220dc39bdbb3ac599342caf36d14250396f
Andre <and.ls.toga@gmail.com>
2025-03-25 17:28:39 -0300

    Realizado alterações para melhor representação do código

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,17 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
 
         int numero = 5;
-
         numero = 10;
-
-        System.out.print(numero);
+        System.out.println(numero);
 
         final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
